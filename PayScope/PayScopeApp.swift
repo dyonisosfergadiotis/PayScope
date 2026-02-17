74e6b91c9a159a0c1fe2c751defa780c70fd8b55
Dyonisos Fergadiotis <dyonisos.fergadiotis@mail.de>
2026-02-17 10:57:40 +0100

    Initial Commit

---
+++ PayScope/PayScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayScopeApp.swift
+//  PayScope
+//
+//  Created by Dyonisos Fergadiotis on 17.02.26.
+//
+
+import SwiftUI
+
+@main
+struct PayScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
